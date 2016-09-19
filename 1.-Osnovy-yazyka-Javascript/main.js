5e1ba12e6e9dcff2833ba82b9390a44b972babf6
Hewlett Packard <Hewlett Packard>
2016-09-19 22:19:18 +0300

    1. Основы языка JS

---
+++ 1.-Osnovy-yazyka-Javascript/main.js
@@ -0,0 +1,15 @@
+// Вывести «Привет, Javascript» c помощью alert
+
+alert("Привет, Javascript");
+
+// Работа с переменными 
+
+var name = "Василий";
+var admin = name;
+
+alert(admin);
+alert(typeof admin);
+
+// Самостоятельно разобраться с атрибутами тэга script (async и defer)
+// Содержимое страницы отбражается одновременно с выполнением скрипта 
+
