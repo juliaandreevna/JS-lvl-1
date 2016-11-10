1675bc411b0438976f03bfa12dafac1920180028
Hewlett Packard <Hewlett Packard>
2016-11-10 22:14:45 +0300

    3. ++

--- 3.-Uslovnye operatory, cicly/main.js
+++ 3.-Uslovnye operatory, cicly/main.js
@@ -2,13 +2,13 @@
 
 var x, str = '';
 for (x = 1; x <= 100; x++) {
-    if ( x % 3 == 0) {
-        console.log('Fizz');   
+    if (x % 3 == 0) {
+        console.log('Fizz');
     } else {
-       if ( x % 5 == 0) {
-           console.log('Buzz');   
+        if (x % 5 == 0) {
+            console.log('Buzz');
         } else {
-        console.log(x);
+            console.log(x);
         }
     }
 }
@@ -17,14 +17,14 @@ for (x = 1; x <= 100; x++) {
 
 var x, str = '';
 for (x = 1; x <= 100; x++) {
-    if ( x % 3 == 0 && x % 5 == 0) {
-        console.log('FizzBuzz');   
+    if (x % 3 == 0 && x % 5 == 0) {
+        console.log('FizzBuzz');
     } else {
-       if ( x % 3 == 0) {
-           console.log('Fizz');   
+        if (x % 3 == 0) {
+            console.log('Fizz');
         } else {
-            if ( x % 5 == 0) {
-                console.log('Buzz');   
+            if (x % 5 == 0) {
+                console.log('Buzz');
             } else {
                 console.log(x);
             }
@@ -47,6 +47,5 @@ console.log(s);
 
 var a, st = '';
 for (a = 1; a <= 8; a++) {
-   console.log(st = st + '#');
+    console.log(st = st + '#');
 }
-
