88f1113b7717e3476170af656143597b5c0a7f36
Hewlett Packard <Hewlett Packard>
2016-11-11 23:09:43 +0300

    4. +з.3, adressbook

---
+++ 4.-Funkcii/adressbook4.js
@@ -0,0 +1,54 @@
+// User storage
+var users = [
+    {
+        firstName: 'Dmitry',
+        lastName: 'Kozlov',
+        birthDay: '16.07.1990',
+        phones: [
+			'89001234567',
+			'83431234567'
+		]
+	},
+    {
+        firstName: 'Ivan',
+        lastName: 'Petrov',
+        birthDay: '08.06.1956',
+        phones: [
+			'89001234567',
+			'83431234567'
+		]
+	}
+];
+
+function addUser(users) {
+    var user = {};
+    user.firstName = prompt('Введите имя:');
+    user.lastName = prompt('Введите фамилию:');
+    user.birthDay = prompt('Дату рождения:');
+    user.phones = [];
+    while (true) {
+        var phone = prompt('Номер телефона:(для выхода введите пустой номер)');
+        if (!phone) break;
+        user.phones.push(phone);
+    }
+    users.push(user);
+}
+
+function csvFormat(users) {
+    return users.map(function (user) {
+        return Object.keys(user).map(function (key) {
+            if (typeof user[key] == 'string' || typeof user[key] == 'number') return '"' + user[key] + '"';
+            else return user[key].map(function (phone) {
+                return '"' + phone + '"';
+            }).join(';');
+        }).join(';');
+    }).join('\r\n');
+}
+
+addUser(users);
+
+for (var i = 0; i < users.length; ++i) {
+    console.log('Имя: ' + users[i].firstName, ' Фамилия: ' + users[i].lastName, ' Дата рождения: ' + users[i].birthDay, ' Телефоны: ' + users[i].phones.join(', '));
+}
+console.log('Вывод CSV:')
+console.log(csvFormat(users));
